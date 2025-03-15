dbe17789cfe806354765f47c747a3ce7bc4166d1
RubaZarea <ruba@accendo.com.my>
2025-03-15 10:32:37 +0300

    Create EAV models and tables

---
+++ app/Models/Attribute.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+
+    protected $fillable = [
+        'name',
+        'type',
+    ];
+
+    const TEXT_TYPE = 'text';
+    const DATE_TYPE = 'date';
+    const NUMBER_TYPE = 'number';
+    const SELECT_TYPE = 'select';
+
+    public static function getAttributeTypes(): array
+    {
+        return [
+            self::TEXT_TYPE,
+            self::DATE_TYPE,
+            self::NUMBER_TYPE,
+            self::SELECT_TYPE
+        ];
+    }
+}

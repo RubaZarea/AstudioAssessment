7e81da4c34ae546a1a5cb83a6bb3f8e208110b41
RubaZarea <ruba@accendo.com.my>
2025-03-17 07:59:31 +0300

    Add database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Attribute;
+use App\Models\AttributeValue;
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Project::factory(10)->create()->each(function ($project) {
+            $users = User::inRandomOrder()->take(rand(1, 3))->pluck('id');
+            $project->users()->attach($users);
+        });
+
+        $attributes = [
+            ['name' => 'department', 'type' => 'text'],
+            ['name' => 'start_date', 'type' => 'date'],
+            ['name' => 'end_date', 'type' => 'date'],
+        ];
+        foreach ($attributes as $attr) {
+            Attribute::create($attr);
+        }
+
+        Project::all()->each(function ($project) {
+            foreach (Attribute::all() as $attribute) {
+                AttributeValue::create([
+                    'attribute_id' => $attribute->id,
+                    'entity_id' => $project->id,
+                    'value' => $attribute->type === 'date' ? now()->toDateString() : 'Dummy Value',
+                ]);
+            }
+        });
     }
 }
